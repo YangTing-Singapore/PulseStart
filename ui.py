7ba232106eab0164badbd0353684e02d0cdbe23b
YangTing <yangtingsingapore@gmail.com>
2021-10-05 22:49:21 +0800

    Regular Update

--- ui.py
+++ ui.py
@@ -8,23 +8,23 @@ class Window(QMainWindow):
     def __init__(self, parent=None):
         """Initializer."""
         super().__init__(parent)
-        self.setWindowTitle("eFuse Measurement")
+        self.setWindowTitle("9SW eFuse Programming Current Measurement")
         self.resize(800, 400)
         self.centralWidget = QLabel("Hello, World")
         self.centralWidget.setAlignment(Qt.AlignHCenter | Qt.AlignVCenter)
         self.setCentralWidget(self.centralWidget)
 
+        self._createMenuBar()
+
+        self.show()
+
     def _createMenuBar(self):
         menuBar = self.menuBar()
-        # Creating menus using a QMenu object
-        fileMenu = QMenu("&File", self)
-        menuBar.addMenu(fileMenu)
-        # Creating menus using a title
+        fileMenu = menuBar.addMenu("&File")
         editMenu = menuBar.addMenu("&Edit")
         helpMenu = menuBar.addMenu("&Help")
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     win = Window()
-    win.show()
     sys.exit(app.exec_())
