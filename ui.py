09136a867b4a6fa330a4496bb0f30e0069310cee
YangTing <yangtingsingapore@gmail.com>
2021-10-06 20:16:11 +0800

    Update ui

--- ui.py
+++ ui.py
@@ -1,28 +1,124 @@
 import sys
 
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QApplication, QLabel, QMainWindow, QMenuBar, QMenu
+from PyQt5.QtWidgets import QApplication, QLabel, QMainWindow, QMenuBar, QMenu, QToolBar, QAction
 
 class Window(QMainWindow):
     """Main Window."""
     def __init__(self, parent=None):
         """Initializer."""
         super().__init__(parent)
-        self.setWindowTitle("9SW eFuse Programming Current Measurement")
+        self.setWindowTitle("eFuse Programming Current Measurement")
         self.resize(800, 400)
         self.centralWidget = QLabel("Hello, World")
         self.centralWidget.setAlignment(Qt.AlignHCenter | Qt.AlignVCenter)
         self.setCentralWidget(self.centralWidget)
 
+        self._createActions()
         self._createMenuBar()
+        self._createToolBars()
+        self._connectActions()
+        self._createStatusBar()
 
         self.show()
 
     def _createMenuBar(self):
         menuBar = self.menuBar()
+        # File menu
         fileMenu = menuBar.addMenu("&File")
+        fileMenu.addAction(self.newAction)
+        fileMenu.addAction(self.openAction)
+        fileMenu.addAction(self.saveAction)
+        fileMenu.addSeparator()
+        fileMenu.addAction(self.exitAction)
+        # Edit meun
         editMenu = menuBar.addMenu("&Edit")
+        editMenu.addAction(self.copyAction)
+        editMenu.addAction(self.pasteAction)
+        editMenu.addAction(self.cutAction)
+        # Find and Replace submenu in the Edit menu
+        findMenu = editMenu.addMenu("Find and Replace")
+        findMenu.addAction("Find...")
+        findMenu.addAction("Replace...")
+        # Help menu
         helpMenu = menuBar.addMenu("&Help")
+        helpMenu.addAction(self.helpContentAction)
+        helpMenu.addAction(self.aboutAction)
+
+    def _createToolBars(self):
+        # Using a title
+        fileToolBar = self.addToolBar("File")
+        fileToolBar.addAction(self.newAction)
+        fileToolBar.addAction(self.openAction)
+        fileToolBar.addAction(self.saveAction)
+
+    def _createActions(self):
+        self.newAction = QAction("&New", self)
+        self.openAction = QAction("&Open...", self)
+        self.saveAction = QAction("&Save", self)
+        self.exitAction = QAction("&Exit", self)
+        self.copyAction = QAction("&Copy", self)
+        self.pasteAction = QAction("&Paste", self)
+        self.cutAction = QAction("C&ut", self)
+        self.helpContentAction = QAction("&Help Content", self)
+        self.aboutAction = QAction("&About", self)
+
+    def _connectActions(self):
+        # Connect File actions
+        self.newAction.triggered.connect(self.newFile)
+        self.openAction.triggered.connect(self.openFile)
+        self.saveAction.triggered.connect(self.saveFile)
+        self.exitAction.triggered.connect(self.close)
+        # Connect Edit actions
+        self.copyAction.triggered.connect(self.copyContent)
+        self.pasteAction.triggered.connect(self.pasteContent)
+        self.cutAction.triggered.connect(self.cutContent)
+        # Connect Help actions
+        self.helpContentAction.triggered.connect(self.helpContent)
+        self.aboutAction.triggered.connect(self.about)
+
+    def getWordCount(self):
+        # Logic for computing the word count goes here...
+        return 42
+
+    def _createStatusBar(self):
+        self.statusbar = self.statusBar()
+        # Adding a temporary message
+        self.statusbar.showMessage("Welcome to eFuse Programming Current Measurement System", 3000)
+        self.wcLabel = QLabel(f"{self.getWordCount()} Words")
+        self.statusbar.addPermanentWidget(self.wcLabel)
+
+    def newFile(self):
+        # Logic for creating a new file goes here...
+        self.centralWidget.setText("<b>File > New</b> clicked")
+
+    def openFile(self):
+        # Logic for opening an existing file goes here...
+        self.centralWidget.setText("<b>File > Open...</b> clicked")
+
+    def saveFile(self):
+        # Logic for saving a file goes here...
+        self.centralWidget.setText("<b>File > Save</b> clicked")
+
+    def copyContent(self):
+        # Logic for copying content goes here...
+        self.centralWidget.setText("<b>Edit > Copy</b> clicked")
+
+    def pasteContent(self):
+        # Logic for pasting content goes here...
+        self.centralWidget.setText("<b>Edit > Paste</b> clicked")
+
+    def cutContent(self):
+        # Logic for cutting content goes here...
+        self.centralWidget.setText("<b>Edit > Cut</b> clicked")
+
+    def helpContent(self):
+        # Logic for launching help goes here...
+        self.centralWidget.setText("<b>Help > Help Content...</b> clicked")
+
+    def about(self):
+        # Logic for showing an about dialog content goes here...
+        self.centralWidget.setText("<b>Help > About...</b> clicked")
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
